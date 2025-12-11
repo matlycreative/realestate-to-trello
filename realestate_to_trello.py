37a671745c11cb06128c74a3bd420a5614b62896
matlycreative <matly.contact@gmail.com>
2025-12-11 11:09:37 +0100

    Update realestate_to_trello.py

--- realestate_to_trello.py
+++ realestate_to_trello.py
@@ -62,9 +62,6 @@ SEEN_FILE        = os.getenv("SEEN_FILE", "seen_domains.txt")
 # Batch rotation
 BATCH_FILE = os.getenv("BATCH_FILE", "batch_state.txt")
 BATCH_SLOTS = [
-    "m tuesday",
-    "a tuesday",
-    "m wednesday",
     "a wednesday",
     "m thursday",
     "a thursday",
@@ -72,6 +69,9 @@ BATCH_SLOTS = [
     "a friday",
     "m monday",
     "a monday",
+    "m tuesday",
+    "a tuesday",
+    "m wednesday",
 ]
 
 def load_batch_index() -> int:
